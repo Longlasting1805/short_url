9891b1b2b34632495271dab8ba2da1d74ac8f095
NEWUSER <longlasting1805@gmail.com>
2022-03-23 08:24:00 +0000

    first commit

--- short_url/settings.py
+++ short_url/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nn+_$u007(5s2adbxtqr2f6=p)bzx#o9y68_tgc=keyon$hkt9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
